9d082559a394bbcde836463b300a9665ccd7f8f7
space-dev-01 <alejandro.work.dev01@gmail.com>
2024-10-16 01:52:49 -0400

    feat: :sparkles: Update app.module with config orm and cache

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ImagesModule } from './modules/images/images.module';
+import { TypeormModule } from './infrastructure/typeorm/typeorm.module';
+import { RedisModule } from '@nestjs-modules/ioredis';
+import { config } from './config/envs';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ImagesModule,
+    TypeormModule,
+    RedisModule.forRoot({
+      type: 'single',
+      url: `redis://${config.REDIS_HOST}:${config.REDIS_PORT}`,
+    }),
+  ],
 })
 export class AppModule {}
