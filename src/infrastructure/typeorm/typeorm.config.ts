fefdeb6e3c922f8cbc3c0cf1ea83ace85026e791
space-dev-01 <alejandro.work.dev01@gmail.com>
2024-10-16 01:50:08 -0400

    feat: :sparkles: Add typeorm module with config db

---
+++ src/infrastructure/typeorm/typeorm.config.ts
@@ -0,0 +1,17 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { config } from '../../config/envs';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+export const getTypeOrmModuleOptions = (): TypeOrmModuleOptions =>
+  ({
+    type: 'postgres',
+    host: config.DB_HOST,
+    port: config.DB_PORT,
+    database: config.DB_NAME,
+    username: config.DB_USER,
+    password: config.DB_PASSWORD,
+    entities: [__dirname + './../../**/*.entity{.ts,.js}'],
+    synchronize: config.DB_SYNCHRONIZE,
+    migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+  }) as TypeOrmModuleOptions;
+
+export default new DataSource(getTypeOrmModuleOptions() as DataSourceOptions);
---
+++ src/infrastructure/typeorm/typeorm.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getTypeOrmModuleOptions } from './typeorm.config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      ...getTypeOrmModuleOptions(),
+    }),
+  ],
+})
+export class TypeormModule {}
