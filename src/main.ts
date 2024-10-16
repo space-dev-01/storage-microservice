2bf4dea0b66216021a8b53bc802f289c594f51da
space-dev-01 <alejandro.work.dev01@gmail.com>
2024-10-16 01:51:06 -0400

    feat: :sparkles: Add doc with swagger and validation pipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from './config/envs';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
+  const openApi = new DocumentBuilder()
+    .setTitle('Image Microservice')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, openApi);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(config.PORT);
 }
 bootstrap();
