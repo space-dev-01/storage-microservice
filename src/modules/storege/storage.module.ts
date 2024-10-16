3fc5aeb8d9ca1c712030d61dfbe9f8c8904ae852
space-dev-01 <alejandro.work.dev01@gmail.com>
2024-10-16 01:51:59 -0400

    feat: :sparkles: Add module storage

---
+++ src/modules/storege/storage.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { FirebaseConfig } from '../../infrastructure/firebase/firebase.service';
+import { StorageService } from './storage.service';
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [FirebaseConfig, StorageService],
+  exports: [StorageService],
+})
+export class StorageModule {}
---
+++ src/modules/storege/storage.service.ts
@@ -0,0 +1,45 @@
+import { ConflictException, Injectable, Logger } from '@nestjs/common';
+import { FirebaseConfig } from '../../infrastructure/firebase/firebase.service';
+
+@Injectable()
+export class StorageService {
+  constructor(private readonly firebaseConfig: FirebaseConfig) {}
+
+  private logger = new Logger(StorageService.name);
+
+  async uploadImageToStorage(
+    buffer: Buffer,
+    fileName: string,
+    mimetype: string,
+  ) {
+    try {
+      const bucket = this.firebaseConfig.getBucket();
+      const fileUpload = bucket.file(fileName);
+
+      await fileUpload.save(buffer, {
+        metadata: {
+          contentType: mimetype,
+        },
+      });
+
+      await fileUpload.makePublic();
+      const publicUrl = `https://storage.googleapis.com/${bucket.name}/${fileName}`;
+
+      return publicUrl;
+    } catch (error) {
+      this.logger.error(error);
+      throw new ConflictException('Error uploading image to storage');
+    }
+  }
+
+  async deletedImageToStorage(fileName: string) {
+    try {
+      const bucket = this.firebaseConfig.getBucket();
+      const file = bucket.file(fileName);
+      await file.delete();
+    } catch (error) {
+      this.logger.error(error);
+      throw new ConflictException('Error deleted image to storage');
+    }
+  }
+}
